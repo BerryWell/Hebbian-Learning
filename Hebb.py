61b9199984a7f2520d7adc72a6c9c91ad431d8a3
Geunwoo Nam <alorse45@gmail.com>
2019-05-23 21:19:57 +0900

    minor change.

--- Hebb.py
+++ Hebb.py
@@ -2,8 +2,8 @@
 # ex) list_in = [+1, -1]
 #     list_weight = [+0.5, -0.5]
 #     out = np.dot(list_in, list_wiehgt)
-#     if(out >= 0) then apple
-#     elif(out < 0) then banana
+#     if(out_1 >= 0 and out_2 < 0) then apple
+#     elif(out_1  < 0 and out_2 >= 0) then banana
 #      |       |
 # -----------------------
 # in_1 | w_11  | w_12
@@ -12,7 +12,7 @@
 # -----------------------
 #      | out_1 | out_2
 # out_1 = in_1*w_11 + in_2*w_21
-# out_2 = in_1*w_12 + in_2*w22
+# out_2 = in_1*w_12 + in_2*w_22
 # 
 # Issue: How to 'train' this algorithm?
 # 1. Generate a weight vector randomly.
@@ -36,11 +36,15 @@ def classfication(list_value):
 	elif(list_value[0] < 0 and list_value[1] >= 0):
 		return 'banana'
 
+def train(list_in, list_weight):
+	pass
+
 list_in = np.array([+1, -1])
 print('Input: %s' % list_in)
 out = Hebb(list_in)
 print('Output for list_in = %s: %s' % (list_in, out))
 
 list_in = np.array([-1, +1])
+print('Input: %s' % list_in)
 out = Hebb(list_in)
 print('Output for list_in = %s: %s' % (list_in, out))
